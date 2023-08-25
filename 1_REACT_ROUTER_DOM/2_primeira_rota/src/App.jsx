d495a6a9978a8dc3799092f634267e6cb9ab2dfa
Francisco <franduda2006@gmail.com>
2023-08-25 14:49:31 -0300

    primeira aula

---
+++ 1_REACT_ROUTER_DOM/2_primeira_rota/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom"
+import  Router  from "./router"
+
+function App() {
+  return (
+    <BrowserRouter>
+     <router />
+    </BrowserRouter>
+  )
+}
+
+export default App
