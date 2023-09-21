ac2215d58c142877dfd5b9d992c5a5880c7b7699
Francisco <franduda2006@gmail.com>
2023-09-21 17:47:06 -0300

    francisco

--- 2_menu_Responsivo/src/compenentes/Header.jsx
+++ 2_menu_Responsivo/src/compenentes/Header.jsx
@@ -1,15 +1,27 @@
 import { List } from "@phosphor-icons/react"
 
+import styles from "./Header.module.css"
+
 function Header() {
   return (
     <header>
         <div className="mobili">
-            <div className="menu_lego">
+            <div className={styles.menu_logo}>
                 <button>
-                  <List size={22} />
+                  <List size={32} />
                 </button>
                 <h1>Logo</h1>
             </div>
+           <div className={styles.opcoes}>
+            <nav>
+            <a href="">Inìcio</a>
+            <a href="">aplicativo</a>
+            <a href="">Componentes</a>
+            <a href="">Artigos</a>
+            <a href="">Criadores</a>
+           </nav>
+          </div>
+           
         </div>
     </header>
   )
