a21386c339e0a6e5d802487ed67e8c35c38dcefa
Francisco <franduda2006@gmail.com>
2023-09-22 15:48:46 -0300

    francisco

--- 2_menu_Responsivo/src/compenentes/Header.jsx
+++ 2_menu_Responsivo/src/compenentes/Header.jsx
@@ -1,18 +1,29 @@
 import { List } from "@phosphor-icons/react"
 
+import { useState } from "react"
+
 import styles from "./Header.module.css"
 
 function Header() {
+  const [abrirMenu, setAbrirMenu] = useState(false)
+
+  function handleAbrirFecharMenu() {
+    if(abrirMenu === true) {
+      setAbrirMenu(false) 
+      return
+    }
+    setAbrirMenu(true)
+  }
   return (
     <header>
-        <div className="mobili">
+        <div className={styles.mobile}>
             <div className={styles.menu_logo}>
-                <button>
+                <button onClick={handleAbrirFecharMenu}>
                   <List size={32} />
                 </button>
                 <h1>Logo</h1>
             </div>
-           <div className={styles.opcoes}>
+          <div className={`${styles.options} ${abrirMenu === true && styles.open}`}>
             <nav>
             <a href="">Inìcio</a>
             <a href="">aplicativo</a>
