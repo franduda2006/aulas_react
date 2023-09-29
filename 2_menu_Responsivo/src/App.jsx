96fff3e75d7194918d565633e79fc8cc25f48365
Francisco <franduda2006@gmail.com>
2023-09-29 15:13:57 -0300

    Marcio

--- 2_menu_Responsivo/src/App.jsx
+++ 2_menu_Responsivo/src/App.jsx
@@ -2,7 +2,13 @@ import Header from "./compenentes/Header"
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex culpa, magnam esse sed adipisci beatae corporis pariatur assumenda praesentium eos dolor amet reiciendis nemo quae quod ducimus quisquam distinctio aspernatur.
+    Saepe, culpa! Fuga velit cum recusandae veritatis dolorum delectus error impedit vero accusamus sapiente sunt ipsa tempore dolore rem quo, voluptate voluptates labore animi laudantium maxime pariatur reprehenderit eligendi esse!
+    Tempore eaque necessitatibus possimus deleniti, placeat vitae itaque similique culpa fuga vero quae cupiditate inventore id hic blanditiis deserunt numquam architecto voluptates doloribus ab, officia assumenda modi. Itaque, esse repudiandae!
+    Ratione dignissimos nulla qui tenetur quaerat pariatur! Enim quae laborum, porro expedita eius placeat perferendis, ipsam veritatis, commodi facilis quia dicta rerum animi laboriosam minima cum ex quas! Dignissimos, inventore.</p>
+    </div>
   )
 }
 
